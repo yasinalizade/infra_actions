9809cfc8de84026afbbdaf5361fac2ef286c37e5
Ясин Ализаде <yasin.alizade@yandex.ru>
2022-06-28 10:45:25 +0300

    django test and flake8 logic

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import include, path 
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
