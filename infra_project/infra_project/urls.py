fa87f4affe5662044d11b5c7379866934f4119b2
Ясин Ализаде <yasin.alizade@yandex.ru>
2022-06-28 10:47:43 +0300

    corrections

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from django.urls import include, path 
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
